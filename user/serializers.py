92f567b6cde8701c0017abba63ad8bf658c9d428
yinmsk <galaxy220111@gmail.com>
2022-06-30 15:52:26 +0900

    ENH 회원가입, 로그인, 로그아웃 기능 구현

---
+++ user/serializers.py
@@ -0,0 +1,20 @@
+from dataclasses import fields
+from django.forms import PasswordInput
+from rest_framework import serializers
+
+from user.models import User as UserModel
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ["username", "password", "fullname", ]
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        user = UserModel(**validated_data)
+        # set_password() 를 사용해 헤싱을 해준다.
+        user.set_password(password)
+        user.save()
+
+        return user
