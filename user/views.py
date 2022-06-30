b41c27972a0d73d63c10dd333e2dbdad7e6a8bd3
yinmsk <galaxy220111@gmail.com>
2022-06-30 21:23:07 +0900

    ENH jwt 로그인 기능 추가

--- user/views.py
+++ user/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions, status
+from user import serializers
 
 from user.serializers import UserSerializer
 
@@ -13,10 +14,15 @@ class UserView(APIView):
     # 회원가입
     def post(self, request):
         user_serializer = UserSerializer(data=request.data)
-        user_serializer.is_valid(raise_exception=True)
-        user_serializer.save()
 
-        return Response(user_serializer.data, status=status.HTTP_200_OK)
+        if user_serializer.is_valid(raise_exception=True):
+            user_serializer.save()
+            return Response({"messages" : "가입 성공"})
+            # return Response(user_serializer.data, status=status.HTTP_200_OK)
+
+        else:
+            print(serializers.errors)
+            return Response({"messages" : "가입 실패"})
 
 
 class UserAPIView(APIView):
