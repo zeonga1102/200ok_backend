07754583563dbe49986beef453d8b043e2225c4e
m_ink <mp98dl4@gmail.com>
2022-07-01 20:37:46 +0900

    ENH serializer, view 수정

--- lounge/serializers.py
+++ lounge/serializers.py
@@ -5,81 +5,82 @@ from dormitory.models import Dormitory
 from django.utils import timezone, dateformat
 
 
+class DormitoriesSerializer(serializers.ModelSerializer):
+    users = serializers.SerializerMethodField(read_only=True)
+
+    def get_users(self, obj):
+        return obj.userinfo_set.filter(dormitory=obj.id).values()
+
+    class Meta:
+        model = Dormitory
+        fields = ["name", "users"]
+
+
 class DormitorySerializer(serializers.ModelSerializer):
+    users = serializers.SerializerMethodField(read_only=True)
 
-        users = serializers.SerializerMethodField(read_only=True)
+    def get_users(self, obj):
+        return obj.userinfo_set.filter(dormitory=obj.id).order_by("?").values()
 
-        def get_users(self, obj):
-                users = obj.userinfo_set.filter(dormitory=obj.id).values()
-
-                for user in users:
-                        portrait = user['portrait']
-                        # 추후 url이 달라질 때 어떻게 자동으로 추적하게 해줘야 하려나?
-                        user['portrait'] = f'/media/{portrait}'
-
-                return users
-
-        class Meta:
-                model = Dormitory
-                fields = ["name", "desc", "logo", "users"]
+    class Meta:
+        model = Dormitory
+        fields = ["name", "desc", "logo", "users"]
 
 
 class BoardSerializer(serializers.ModelSerializer):
+    icon = serializers.SerializerMethodField(read_only=True)
 
-        icon = serializers.SerializerMethodField(read_only=True)
+    def get_icon(self, obj):
+        try:
+            dorm_name = obj.author.userinfo.dormitory
+            dorm = Dormitory.objects.get(name=dorm_name)
+            icon = dorm.logo.url
+            return icon
+        except:
+            return ''
 
-        def get_icon(self, obj):
-                try:
-                        dorm_name = obj.author.userinfo.dormitory
-                        dorm = Dormitory.objects.get(name=dorm_name)
-                        icon = dorm.logo.url
-                        return icon
-                except:
-                        return ''
-        
-        def validate(self, data):
-                if len(data.get('content')) <= 5:
-                         raise serializers.ValidationError(
-                                detail={"error": "5글자를 넘아야 합니다."},
-                                )
+    def validate(self, data):
+        if len(data.get('content')) <= 5:
+            raise serializers.ValidationError(
+                detail={"error": "5글자를 넘아야 합니다."},
+            )
 
-                return data
+        return data
 
-        def create(self, validated_data):
-                cemment = Board(**validated_data)
-                cemment.save()
+    def create(self, validated_data):
+        cemment = Board(**validated_data)
+        cemment.save()
 
-                return validated_data
+        return validated_data
 
-        def update(self, instance, validated_data):
-                formatted_date = dateformat.format(timezone.now(), 'y.m.d H:i')
+    def update(self, instance, validated_data):
+        formatted_date = dateformat.format(timezone.now(), 'y.m.d H:i')
 
-                for key, value in validated_data.items():
-                        if key == 'content':
-                                value = value + f' ({formatted_date} 수정)'
-                                setattr(instance, key, value)
-                instance.save()
+        for key, value in validated_data.items():
+            if key == 'content':
+                value = value + f' ({formatted_date} 수정)'
+                setattr(instance, key, value)
+        instance.save()
 
-                return instance
+        return instance
 
-        class Meta:
-                model = Board
-                fields = "__all__"
+    class Meta:
+        model = Board
+        fields = "__all__"
 
 
 class LoungeSerializer(serializers.ModelSerializer):
+    portrait = serializers.SerializerMethodField(read_only=True)
+    dormitory_name = serializers.SerializerMethodField(read_only=True)
 
-        portrait = serializers.SerializerMethodField(read_only=True)
-        dormitory_name = serializers.SerializerMethodField(read_only=True)
+    def get_portrait(self, obj):
+        portrait = obj.userinfo.portrait.url
+        return portrait
 
-        def get_portrait(self, obj):
-                portrait = obj.userinfo.portrait.url
-                return portrait
+    def get_dormitory_name(self, obj):
+        dormitory_name = obj.userinfo.dormitory.name
+        return dormitory_name
 
-        def get_dormitory_name(self, obj):
-                dormitory_name = obj.userinfo.dormitory.name
-                return dormitory_name
-
-        class Meta:
-                model = User
-                fields = ["id", "username", "fullname", "portrait", "dormitory_name"]
+    class Meta:
+        model = User
+        fields = ["id", "username", "fullname", "portrait", "dormitory_name"]
--- lounge/views.py
+++ lounge/views.py
@@ -1,77 +1,85 @@
-import re
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions, status
 
-from .serializers import BoardSerializer, LoungeSerializer, DormitorySerializer
+from .serializers import BoardSerializer, LoungeSerializer, DormitorySerializer, DormitoriesSerializer
 from .models import Board
 from user.models import User, UserInfo
 from dormitory.models import Dormitory
 
 
-# Create your views here.
 class LoungeView(APIView):
-        permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
-        # TODO  라운지 페이지 띄우기
-        def get(self, request):
-                user = request.user
-                userinfo = User.objects.filter(id=user.id)
-                userdorm = Dormitory.objects.filter(name=user.userinfo.dormitory.name)
-                dorm = Dormitory.objects.all()
-                dorm_count = dorm.count()
-                board = Board.objects.all().order_by('-created')
+    # TODO  라운지 페이지 띄우기
+    def get(self, request):
+        user = request.user
+        userinfo = User.objects.filter(id=user.id)
+        userdorm = Dormitory.objects.filter(name=user.userinfo.dormitory.name)
+        dorm = Dormitory.objects.all()
+        dorm_count = dorm.count()
+        board = Board.objects.all().order_by('-created')
 
-                lounge_serialrzer = LoungeSerializer(userinfo, many=True).data
-                dormitory_serializer = DormitorySerializer(userdorm, many=True).data
-                dormitories_serializer = DormitorySerializer(dorm, many=True).data
-                board_serializer = BoardSerializer(board, many=True).data
+        lounge_serialrzer = LoungeSerializer(userinfo, many=True).data
+        dormitory_serializer = DormitorySerializer(userdorm, many=True).data
+        dormitories_serializer = DormitoriesSerializer(dorm, many=True).data
+        board_serializer = BoardSerializer(board, many=True).data
 
-                dorm_name_list = [ dormitories_serializer[i]['name'] for i in range(dorm_count) ]
-                dorm_users_count_list =  [ dormitories_serializer[i]['users'].count() for i in range(dorm_count) ]
+        dorm_name_list = [dormitories_serializer[i]['name'] for i in range(dorm_count)]
+        dorm_users_count_list = [dormitories_serializer[i]['users'].count() for i in range(dorm_count)]
+                        
+        dormitory_serializer[0]['users'] = dormitory_serializer[0]['users'].exclude(user_id=user.id)
 
-                lounge_data = {
-                        "user": lounge_serialrzer,
-                        "house": dormitory_serializer,
-                        "count":{
-                                "label":dorm_name_list, 
-                                "data":dorm_users_count_list
-                                },
-                        "board":board_serializer
-                }
+        if len(dormitory_serializer[0]['users']) >= 5:
+                dormitory_serializer[0]['users'] = dormitory_serializer[0]['users'][:5]
 
-                return Response(lounge_data, status=status.HTTP_200_OK)
+        for user in dormitory_serializer[0]['users']:
+                portrait = user['portrait']
+                # 추후 url이 달라질 때 어떻게 자동으로 추적하게 해줘야 하려나?
+                user['portrait'] = f'/media/{portrait}'
 
-        # TODO 게시글 쓰기
-        def post(self, request):
-                request.data['author'] = request.user.id
-                board_serializer = BoardSerializer(data=request.data)
+        lounge_data = {
+            "user": lounge_serialrzer,
+            "house": dormitory_serializer,
+            "count": {
+                "label": dorm_name_list,
+                "data": dorm_users_count_list
+            },
+            "board": board_serializer
+        }
 
-                if board_serializer.is_valid():
-                        board_serializer.save()
-                        return Response({"message": "정상"}, status=status.HTTP_200_OK)
-        
-                return Response(board_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(lounge_data, status=status.HTTP_200_OK)
 
-        # TODO 게시글 수정
-        def put(self, request, obj_id):
-                comment = Board.objects.get(id=obj_id)
-                board_serializer = BoardSerializer(comment, data=request.data, partial=True)
-                print(f'author: {comment.author}, user: {request.user.username}')
+    # TODO 게시글 쓰기
+    def post(self, request):
+        request.data['author'] = request.user.id
+        board_serializer = BoardSerializer(data=request.data)
 
-                if comment.author == request.user:
-                        if board_serializer.is_valid():
-                                board_serializer.save()
-                                return Response({"message": "정상"}, status=status.HTTP_200_OK) 
-                        return Response(board_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-                return Response({'message':'이 글을 작성한 사람이 아닙니다!'}, status=status.HTTP_400_BAD_REQUEST)
+        if board_serializer.is_valid():
+            board_serializer.save()
+            return Response({"message": "정상"}, status=status.HTTP_200_OK)
 
-        # TODO 게시글 삭제
-        def delete(self, request, obj_id):
-                comment = Board.objects.get(id=obj_id)
+        return Response(board_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-                if comment.author == request.user:
-                        comment.delete()
-                        return Response({"message": "정상"}, status=status.HTTP_200_OK)
-                
-                return Response({'message':'이 글을 작성한 사람이 아닙니다!'}, status=status.HTTP_400_BAD_REQUEST)
+    # TODO 게시글 수정
+    def put(self, request, obj_id):
+        comment = Board.objects.get(id=obj_id)
+        board_serializer = BoardSerializer(comment, data=request.data, partial=True)
+        print(f'author: {comment.author}, user: {request.user.username}')
+
+        if comment.author == request.user:
+            if board_serializer.is_valid():
+                board_serializer.save()
+                return Response({"message": "정상"}, status=status.HTTP_200_OK)
+            return Response(board_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': '이 글을 작성한 사람이 아닙니다!'}, status=status.HTTP_400_BAD_REQUEST)
+
+    # TODO 게시글 삭제
+    def delete(self, request, obj_id):
+        comment = Board.objects.get(id=obj_id)
+
+        if comment.author == request.user:
+            comment.delete()
+            return Response({"message": "정상"}, status=status.HTTP_200_OK)
+
+        return Response({'message': '이 글을 작성한 사람이 아닙니다!'}, status=status.HTTP_400_BAD_REQUEST)
