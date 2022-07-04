bdc729eb135da4efc824ec01a1cdbdac302494e3
LULULALA2 <lhg9714@gmail.com>
2022-07-04 20:09:27 +0900

    ENH: modify dormitory view (API 통합)

--- dormitory/views.py
+++ dormitory/views.py
@@ -3,23 +3,14 @@ from rest_framework.views import APIView
 from rest_framework import status
 from django.shortcuts import render
 from dormitory.serializers import DormUserSerializer
-from user.models import UserInfo
-
-
-# Create your views here.
-class MyRoomView(APIView):
-    def get(self, request):
-        user = request.user
-
-        return Response(DormUserSerializer(user).data)
+from user.models import User, UserInfo
 
 
 class DormitoryView(APIView):
     def get(self, request):
         user = request.user
-        dormitory = UserInfo.objects.get(user=user).dormitory
-        print(dormitory.name, dormitory.id)
+        # user = User.objects.get(id=6)
+        # dormitory = UserInfo.objects.get(user=user).dormitory
+        # print(dormitory.name, dormitory.id)
 
-        return Response({'dormitory_name': dormitory.name, 'dormitory_id': dormitory.id})
-        # return render(request, 'room/dormitory.html', {'dormitory': dormitory})
-
+        return Response(DormUserSerializer(user).data)
--- ok200/settings.py
+++ ok200/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 추가한 app
+    'corsheaders',
     'rest_framework',
     'dormitory',
     'user',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'user.User' # app.table 형태
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -148,3 +150,8 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.MultiPartParser'
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+]
