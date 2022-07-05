f7f65990949d58f08aeee2d3e3f23a4d2765b50e
m_ink <mp98dl4@gmail.com>
2022-07-05 21:13:21 +0900

    BUG 에러 수정 및 시리얼라이저 수정

--- lounge/urls.py
+++ lounge/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('post/', views.LoungeView.as_view(), name='comment_post'),
     path('edit/<int:obj_id>/', views.LoungeView.as_view(), name='comment_edit'),
     path('delete/<int:obj_id>/', views.LoungeView.as_view(), name='comment_delete'),
-    # path('<int:obj_id>/', views.LoungeView.as_view(), name='comment_update'),
 ]
